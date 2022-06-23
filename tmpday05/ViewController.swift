5f51a620481fbc7a508d66af4837188978c0d29c
Kiyohiro Honda <kiyohiro.honda@nex.nikkei.com>
2022-06-24 01:40:26 +0900

    update: グループのリストアップまでは完成した 今後のto-do スクロールして手を話すともとの位置に戻ってしまう。これだと、リストが多い場合にリストの隅にあるセルが選択できない。 削除の実装

--- tmpday05/ViewController.swift
+++ tmpday05/ViewController.swift
@@ -12,11 +12,21 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
 
     var containers: [CNContainer] = []
     var groups: [CNGroup] = []
-    var containersArray: [String] = []
-    var groupsArray: [String] = []
     let tableView = UITableView()
     let button1 =  UIButton(type: UIButton.ButtonType.system)
-
+    
+    struct ContactsFolder {
+        enum FolderType {
+            case container
+            case group
+            case voidGroup
+        }
+        var folderType: FolderType
+        var id: String
+        var nameOfContainer: String
+        var nameOfGroup: String
+    }
+    var contactsFolders: [ContactsFolder] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +39,108 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         refreshGroupTable()
     }
 
+    
+    
+    func displayUI(){
+        let statusBarHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+
+        button1.frame = CGRect(x: 40, y:50, width: 150, height:50)
+        button1.frame.origin.x = self.view.frame.width/2 - button1.frame.size.width/2
+        button1.setTitle("Clean Up!", for: UIControl.State.normal)
+        button1.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        button1.titleLabel?.adjustsFontSizeToFitWidth = true
+        button1.layer.cornerRadius = 10
+        button1.layer.borderWidth = 1
+        self.view.addSubview(button1)
+        button1.addTarget(self, action: #selector(self.deleteRecords(sender:)), for: .touchUpInside)
+
+        tableView.frame = CGRect(
+            x: 0,
+            y: statusBarHeight + 50 + button1.frame.height,
+            width: self.view.frame.width,
+            height: self.view.frame.height - statusBarHeight
+        )
+        self.view.addSubview(tableView)
+        // Delegate設定
+        tableView.delegate = self
+        // DataSource設定
+        tableView.dataSource = self
+        // 画面に UITableView を追加
+    }
+    
+    func refreshGroupTable(){
+        /*
+         contactstore1にアクセスして、各コンテナ内のグループを
+         cellMainTitlesとcellSubTilesに設定する
+        */
+
+        let store = CNContactStore()
+        do {
+            containers = try store.containers(matching: nil)
+        } catch{
+            print(error)
+        }
+        print("Number of containers is \(containers.count)")
+        contactsFolders = []
+        for container in containers {
+            print("container is \(container)")
+            let fetchPredicate = CNGroup.predicateForGroupsInContainer(withIdentifier: container.identifier)
+            do {
+                let groupsInOneContainer = try store.groups(matching: fetchPredicate)
+                if (groupsInOneContainer.count == 0) {
+                    // コンテナの中にグループが存在しないタイプ（ex. Exchange）
+                    contactsFolders.append(ContactsFolder(folderType: .container, id: container.identifier, nameOfContainer: container.name, nameOfGroup: "グループ無し"))
+                } else{
+                    // コンテナの中にグループが存在するタイプ　（ex. icloudや端末ローカル）
+                    for group in groupsInOneContainer {
+                        contactsFolders.append(ContactsFolder(folderType: .group, id: group.identifier, nameOfContainer: container.name, nameOfGroup: group.name))
+                    }
+                    // グループが存在するタイプはグループに属していない連絡先群も存在するだろう
+                    contactsFolders.append(ContactsFolder(folderType: .voidGroup, id: "Dummy", nameOfContainer: container.name, nameOfGroup: "グループに属していない連絡先"))
+                }
+            } catch {
+                print("Error fetching in containers")
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    
+    @objc
+    func deleteRecords(sender: Any){
+        print("function deleteRecords is called")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return groupsArray.count
+        return contactsFolders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 一つ一つのセルを作る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.accessoryType = .checkmark
+//        cell.textLabel?.text = groupsArray[indexPath.row]
+        cell.textLabel?.text = contactsFolders[indexPath.row].nameOfGroup
+        cell.detailTextLabel?.text = contactsFolders[indexPath.row].nameOfContainer != "" ? contactsFolders[indexPath.row].nameOfContainer : "ローカル"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // セルがタップされた時の処理
+        print("タップされたセルのindex番号は: \(indexPath.row)")
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        // アクセサリボタン（セルの右にあるボタン）がタップされた時の処理
+        // この関数は、アクセサリの種類が.checkmarkの場合は呼ばれないので、
+        // 内容は空でも良いのだが、将来種類を変えた時のために便宜上書いておく。
+        print("タップされたアクセサリがあるセルのindex番号: \(indexPath.row)")
+    }
+
+    
     func checkAuthorizationStatus(){
         // 連絡帳へアクセスできることを確認し、もし出来ないならばユーザーに出来るようにせよと催す
         let status = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
@@ -65,108 +177,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         @unknown default:
             fatalError()
         }
-        
-    }
-    
-    
-    func displayUI(){
-        let statusBarHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
-
-        button1.frame = CGRect(x: 40, y:50, width: 150, height:50)
-        button1.frame.origin.x = self.view.frame.width/2 - button1.frame.size.width/2
-        button1.setTitle("Clean Up!", for: UIControl.State.normal)
-        button1.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button1.titleLabel?.adjustsFontSizeToFitWidth = true
-        button1.layer.cornerRadius = 10
-        button1.layer.borderWidth = 1
-        self.view.addSubview(button1)
-        button1.addTarget(self, action: #selector(self.deleteRecords(sender:)), for: .touchUpInside)
-
-        tableView.frame = CGRect(
-            x: 0,
-            y: statusBarHeight + 50 + button1.frame.height,
-            width: self.view.frame.width,
-            height: self.view.frame.height - statusBarHeight
-        )
-        self.view.addSubview(tableView)
-        // Delegate設定
-        tableView.delegate = self
-        // DataSource設定
-        tableView.dataSource = self
-        // 画面に UITableView を追加
-    }
-    
-    func refreshGroupTable(){
-        /*
-         contactstore1にアクセスして、各コンテナ内のグループを
-         cellMainTitlesとcellSubTilesに設定する
-        */
-//      いまはダミーコードを入れておく
-//        groupsArray = ["g0c0", "g1c0", "g2c0", "g0c1", "g1c1"]
-//        containersArray  = ["c0", "c0", "c0", "c1", "c1"]
-        let store = CNContactStore()
-        do {
-            containers = try store.containers(matching: nil)
-        } catch{
-            print(error)
-        }
-        print("Number of containers is \(containers.count)")
-        for container in containers {
-            print("container is \(container)")
-            let fetchPredicate = CNGroup.predicateForGroupsInContainer(withIdentifier: container.identifier)
-            do {
-                let groupsInOneContainer = try store.groups(matching: fetchPredicate)
-                if (groupsInOneContainer.count == 0) {
-                    containersArray.append(container.name != "" ? container.name : "local")
-                    groupsArray.append("グループ無し")
-                } else{
-                    for group in groupsInOneContainer {
-                        containersArray.append(container.name != "" ? container.name : "local")
-                        groupsArray.append(group.name)
-                    }
-
-                }
-            } catch {
-                print("Error fetching in containers")
-            }
-        }
-        
-        
-        tableView.reloadData()
-    }
-    
-    
-    @objc
-    func deleteRecords(sender: Any){
-        print("function deleteRecords is called")
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groupsArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 一つ一つのセルを作る
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        cell.accessoryType = .checkmark
-        cell.textLabel?.text = groupsArray[indexPath.row]
-        cell.detailTextLabel?.text = containersArray[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // セルがタップされた時の処理
-        print("タップされたセルのindex番号は: \(indexPath.row)")
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-    }
-    
-    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
-        // アクセサリボタン（セルの右にあるボタン）がタップされた時の処理
-        // この関数は、アクセサリの種類が.checkmarkの場合は呼ばれないので、
-        // 内容は空でも良いのだが、将来種類を変えた時のために便宜上書いておく。
-        print("タップされたアクセサリがあるセルのindex番号: \(indexPath.row)")
     }
 
+    
 }
-
